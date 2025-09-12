04d0e9a66f6cd0884153836319e4c140b4cff26e
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-09-13 00:19:18 +0530

    Refactor public key retrieval to use PemReader and add Bouncy Castle dependencies

--- src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/device/DeviceExtensions.kt
+++ src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/device/DeviceExtensions.kt
@@ -1,13 +1,16 @@
 package com.srilakshmikanthanp.clipbirdhub.device
 
+import org.bouncycastle.util.io.pem.PemReader
+import java.io.StringReader
 import java.security.KeyFactory
 import java.security.PublicKey
 import java.security.spec.X509EncodedKeySpec
-import java.util.Base64
 
 fun Device.getPublicKey(): PublicKey {
-  val bytes = Base64.getDecoder().decode(this.publicKey)
-  val spec = X509EncodedKeySpec(bytes)
-  val keyFactory = KeyFactory.getInstance(DeviceConstants.keyAlgorithm)
-  return keyFactory.generatePublic(spec)
+  PemReader(StringReader(this.publicKey)).use { reader ->
+    val content = reader.readPemObject().content
+    val spec = X509EncodedKeySpec(content)
+    val keyFactory = KeyFactory.getInstance(DeviceConstants.keyAlgorithm)
+    return keyFactory.generatePublic(spec)
+  }
 }
