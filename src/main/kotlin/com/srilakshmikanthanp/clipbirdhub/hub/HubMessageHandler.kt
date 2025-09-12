fb3f8e4763178fe883aab97b3ca0c61c8f9228ae
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-09-13 00:23:57 +0530

    Refactor HubMessageHandler and HubMessageValidator as non abstraction based

--- src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/hub/HubMessageHandler.kt
+++ src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/hub/HubMessageHandler.kt
@@ -1,5 +1,22 @@
 package com.srilakshmikanthanp.clipbirdhub.hub
 
-interface HubMessageHandler {
-  fun handle(session: HubSession, message: HubMessage<out HubMessagePayload>)
+import org.springframework.stereotype.Service
+import kotlin.reflect.full.findAnnotation
+
+@Service
+class HubMessageHandler(
+  private val hubMessageValidator: HubMessageValidator,
+  payloadHandlers: List<HubMessagePayloadHandler<HubMessagePayload>>
+) {
+  private val payloadHandlers: Map<Class<out HubMessagePayload>, HubMessagePayloadHandler<HubMessagePayload>> = payloadHandlers.filter {
+    it::class.findAnnotation<HubMessageHandling>() != null
+  }.associateBy {
+    it.payloadType
+  }
+
+  fun handle(session: HubSession, message: HubMessage<out HubMessagePayload>) {
+    hubMessageValidator.validate(session, message)
+    val handler = payloadHandlers[message.payload::class.java] ?: throw IllegalArgumentException("No handler found for payload type: ${message.payload::class.java}")
+    handler.handle(session, message.payload)
+  }
 }
--- src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/hub/HubMessageValidator.kt
+++ src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/hub/HubMessageValidator.kt
@@ -1,5 +1,19 @@
 package com.srilakshmikanthanp.clipbirdhub.hub
 
-interface HubMessageValidator {
-  fun validate(session: HubSession, message: HubMessage<out HubMessagePayload>)
+import org.springframework.stereotype.Service
+import kotlin.reflect.full.findAnnotation
+
+@Service
+class HubMessageValidator(
+  payloadValidators: List<HubMessagePayloadValidator<HubMessagePayload>>
+) {
+  private val payloadValidators: Map<Class<out HubMessagePayload>, HubMessagePayloadValidator<HubMessagePayload>> = payloadValidators.filter {
+    it::class.findAnnotation<HubMessageValidation>() != null
+  }.associateBy {
+    it.payloadType
+  }
+
+  fun validate(session: HubSession, message: HubMessage<out HubMessagePayload>) {
+    payloadValidators[message.payload::class.java]?.validate(session, message.payload)
+  }
 }
