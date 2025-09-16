2c40e305702e73f23374c15bdc09972a2e633fc2
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-09-16 11:47:43 +0530

    Update MapperAppConfig to disable builder in MapStruct configuration

--- src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/common/config/MapperAppConfig.kt
+++ src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/common/config/MapperAppConfig.kt
@@ -1,16 +1,14 @@
 package com.srilakshmikanthanp.clipbirdhub.common.config
 
-import org.mapstruct.InjectionStrategy
-import org.mapstruct.MapperConfig
-import org.mapstruct.MappingInheritanceStrategy
-import org.mapstruct.NullValuePropertyMappingStrategy
+import org.mapstruct.*
 import org.mapstruct.extensions.spring.SpringMapperConfig
 
 @MapperConfig(
   mappingInheritanceStrategy = MappingInheritanceStrategy.AUTO_INHERIT_FROM_CONFIG,
   componentModel = "spring",
   injectionStrategy = InjectionStrategy.CONSTRUCTOR,
-  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+  builder = Builder(disableBuilder = true)
 )
 @SpringMapperConfig
 interface MapperAppConfig
