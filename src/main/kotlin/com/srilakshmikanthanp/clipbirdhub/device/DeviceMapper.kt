64275ab7ae1988937289b51c1e7f460c55de9652
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-09-16 11:48:34 +0530

    Rename parameter for online status in DeviceMapper to improve clarity

--- src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/device/DeviceMapper.kt
+++ src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/device/DeviceMapper.kt
@@ -11,12 +11,12 @@ interface DeviceMapper {
   @Mapping(source = "req.publicKey", target = "publicKey")
   @Mapping(source = "req.name", target = "name")
   @Mapping(source = "req.type", target = "type")
-  @Mapping(source = "online", target = "online")
+  @Mapping(source = "isOnline", target = "online")
   @Mapping(source = "user", target = "user")
   @Mapping(target = "id", ignore = true)
   @Mapping(target = "createdAt", ignore = true)
   @Mapping(target = "updatedAt", ignore = true)
-  fun create(req: DeviceRequestDto, online: Boolean,  user: User): Device
+  fun create(req: DeviceRequestDto, isOnline: Boolean,  user: User): Device
 
   @Mapping(source = "publicKey", target = "publicKey")
   @Mapping(source = "name", target = "name")
