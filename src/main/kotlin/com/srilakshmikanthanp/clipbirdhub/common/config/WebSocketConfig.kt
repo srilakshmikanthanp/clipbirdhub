65dc9c9d21f2c8b989bec3fb44046f546727be7b
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-09-25 07:45:11 +0530

    Add WebSocket configuration with custom buffer sizes

--- src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/common/config/WebSocketConfig.kt
+++ src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/common/config/WebSocketConfig.kt
@@ -1,10 +1,12 @@
 package com.srilakshmikanthanp.clipbirdhub.common.config
 
 import com.srilakshmikanthanp.clipbirdhub.hub.HubWebSocketServer
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean
 
 @Configuration
 @EnableWebSocket
@@ -13,5 +15,14 @@ class WebSocketConfig(
 ): WebSocketConfigurer {
   override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
     registry.addHandler(hubWebSocketServer, "hub")
+  }
+
+  @Bean
+  fun createWebSocketContainer(): ServletServerContainerFactoryBean {
+    val container = ServletServerContainerFactoryBean()
+    val size = 10 * 1024 * 1024 // 10 MB
+    container.setMaxTextMessageBufferSize(size)
+    container.setMaxBinaryMessageBufferSize(size)
+    return container
   }
 }
