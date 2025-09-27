4718a47a5fbfe27e72a69697b90d4ff62b70eab9
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-09-27 17:43:05 +0530

    Fix updating device status on joining/leaving the hub

--- src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/hub/HubEventHandlerImpl.kt
+++ src/main/kotlin/com/srilakshmikanthanp/clipbirdhub/hub/HubEventHandlerImpl.kt
@@ -2,6 +2,7 @@ package com.srilakshmikanthanp.clipbirdhub.hub
 
 import com.srilakshmikanthanp.clipbirdhub.common.extensions.deviceToDeviceResponseDto
 import com.srilakshmikanthanp.clipbirdhub.common.extensions.toHubMessage
+import com.srilakshmikanthanp.clipbirdhub.device.DeviceRepository
 import org.springframework.core.convert.ConversionService
 import org.springframework.stereotype.Service
 
@@ -10,9 +11,12 @@ class HubEventHandlerImpl(
   private val hubMessageHandler: HubMessageHandler,
   private val hubDeviceSessionRegistry: HubDeviceSessionRegistry,
   private val conversionService: ConversionService,
+  private val deviceRepository: DeviceRepository,
 ): HubEventHandler {
   override fun afterConnectionEstablished(session: HubSession) {
-    val deviceAddedPayload = HubMessageDeviceAddedPayload(conversionService.deviceToDeviceResponseDto(session.getDevice()))
+    val device = session.getDevice().apply { this.isOnline = true }
+    deviceRepository.save(device)
+    val deviceAddedPayload = HubMessageDeviceAddedPayload(conversionService.deviceToDeviceResponseDto(device))
     val deviceAddedHubMessage = deviceAddedPayload.toHubMessage()
     val userDevices = hubDeviceSessionRegistry.getAllSessionsByUser(session.getUser()).map { conversionService.deviceToDeviceResponseDto(it.getDevice()) }
     val devicesPayload = HubMessageDevicesPayload(userDevices)
@@ -28,7 +32,9 @@ class HubEventHandlerImpl(
   }
 
   override fun afterConnectionClosed(session: HubSession, code: Int, reason: String?) {
-    val deviceLeftPayload = HubMessageDeviceRemovedPayload(conversionService.deviceToDeviceResponseDto(session.getDevice()))
+    val device = session.getDevice().apply { this.isOnline = false }
+    deviceRepository.save(device)
+    val deviceLeftPayload = HubMessageDeviceRemovedPayload(conversionService.deviceToDeviceResponseDto(device))
     val hubMessage = deviceLeftPayload.toHubMessage()
     hubDeviceSessionRegistry.unregister(session)
     hubDeviceSessionRegistry.getAllSessionsByUser(session.getUser()).forEach { it.sendMessage(hubMessage) }
